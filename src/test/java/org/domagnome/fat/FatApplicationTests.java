d5091a3af509cad0fdf42c76e23d2f9917207ab0
domagnome <domagnome@ya.ru>
2024-03-20 22:14:58 +0300

    Initioal commit

---
+++ src/test/java/org/domagnome/fat/FatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.domagnome.fat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
